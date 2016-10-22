9fdf943cd84f6877a90a14937e3eca1bc5c6f8aa
lanreogunmola@github.com <lanreogunmola@gmail.com>
2016-10-22 16:45:08 -0700

    route and boostrap installed

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('ecommerce', function() {
+    this.route('product');
+  });
 });
 
 export default Router;
---
+++ ember-cli-build.js
@@ -0,0 +1,11 @@
+/*jshint node:true*/
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+	var app = new EmberApp(defaults, {
+
+	});
+
+	app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+	return app.toTree();
+};
